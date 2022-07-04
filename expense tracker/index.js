bef0030a3fe269b88e8928e1775a5811833f041b
Abhishek Chaturvedi <65055220+ABHCH@users.noreply.github.com>
2022-07-05 00:23:18 +0530

    Update index.js

--- expense tracker/index.js
+++ expense tracker/index.js
@@ -1,4 +1,5 @@
 // selectors
+// selectors
 
 const labeluserName = document.querySelector('.user-name');
 const labeluserPassword = document.querySelector('.user-password');
@@ -6,6 +7,13 @@ const loginBtn = document.querySelector('.login-btn');
 const loginContainer = document.querySelector('.login-container');
 const app = document.querySelector('.app');
 const logoutBtn = document.querySelector('.logout-btn');
+const addTrans = document.querySelector('.add-trans');
+const incomeBtn = document.querySelector('.income-btn');
+const expenseBtn = document.querySelector('.expense-btn');
+const amountValue = document.querySelector('.amount-value');
+const typeValue = document.querySelector('.type-amount');
+const iconMark = document.getElementById('icon-mark');
+const modalWindow = document.querySelector('.modal-window');
 
 const account = {
     userName:'ABHCH',
@@ -33,4 +41,13 @@ logoutBtn.addEventListener("click", () => {
     app.style.opacity = '-1'
     loginContainer.style.opacity = `1`
     
+})
+
+addTrans.addEventListener("click", () => {
+   modalWindow.classList.add('open-modal')
+})
+
+iconMark.addEventListener("click", () => {
+    modalWindow.classList.remove('open-modal')
+
 })
